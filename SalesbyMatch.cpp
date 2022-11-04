91c05a80664aa5ce2b08b3be89c605290bbbc9ac
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-11-04 18:21:22 +0900

    Update SalesbyMatch.cpp

--- SalesbyMatch.cpp
+++ SalesbyMatch.cpp
@@ -1,3 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
 int sockMerchant(int n, vector<int> ar)
 {
     int result = 0;
